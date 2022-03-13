9bfcd93503f6b4a1eca1d68129681bb5f9ce3e64
Ivan Jones <visionaryCoder@live.com>
2022-03-12 18:43:10 -0800

    Syncing between machines

---
+++ vc/Accessor/Game/Service/GameAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vc.Accessor.Game.Service
+{
+
+    interface IGameAccess
+    {
+
+    }
+
+    internal class GameAccess : IGameAccess
+    {
+
+    }
+
+}
